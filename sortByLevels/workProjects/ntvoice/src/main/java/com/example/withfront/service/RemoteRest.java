3cadd23d16af50360ce90dd5748242ea1a8e74f1
armantulendi <54793232+armantulendi@users.noreply.github.com>
2021-05-09 23:32:03 +0600

    09052021

--- sortByLevels/workProjects/ntvoice/src/main/java/com/example/withfront/service/RemoteRest.java
+++ sortByLevels/workProjects/ntvoice/src/main/java/com/example/withfront/service/RemoteRest.java
@@ -1,54 +1,65 @@
 package com.example.withfront.service;
 
-import com.example.withfront.model.MyResponse;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import net.minidev.json.parser.JSONParser;
-import org.apache.catalina.connector.Response;
-import org.apache.tomcat.util.json.ParseException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.json.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.*;
 import java.nio.charset.StandardCharsets;
 
 @Service
 public class RemoteRest {
-    public String getResponse(String method) throws IOException {
-        URL url = new URL ("http://192.168.17.81:8080/RPC");
-        HttpURLConnection con = (HttpURLConnection)url.openConnection();
-        StringBuilder response = new StringBuilder();
-        con.setRequestMethod("POST");
+    @Value("${sip.proxy1.ip}")
+    String sipProxy1;
 
-        JSONParser jsonParser;
-        MyResponse jsonpObject = null;
-        con.setRequestProperty("Content-Type", "application/json; utf-8");
-        con.setRequestProperty("Accept", "application/json");
-        con.setDoOutput(true);
-        String jsonInputString = "{\"jsonrpc\": \"2.0\", \"method\":\"dlg.stats_active\"}";
-        try(OutputStream os = con.getOutputStream()) {
+    public String getResponse(String method )   {
+        HttpURLConnection con;
+        URL url ;
+        String jsonInputString;
+        BufferedReader br;
+        String responseLine;
+        StringBuilder response= new StringBuilder();
+        try {
+            url = new URL("http://"+ sipProxy1 +":8080/RPC");
+            con = (HttpURLConnection)url.openConnection();
+            con.setRequestMethod("POST");
+            con.setRequestProperty("Content-Type", "application/json; utf-8");
+            con.setRequestProperty("Accept", "application/json");
+            con.setDoOutput(true);
+            jsonInputString = "{\"jsonrpc\": \"2.0\", \"method\":\"" + method + "\"}";
+            OutputStream os = con.getOutputStream();
             byte[] input = jsonInputString.getBytes(StandardCharsets.UTF_8);
             os.write(input, 0, input.length);
-        }
-        try(BufferedReader br = new BufferedReader(
-                new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8))) {
-            String responseLine ;
+            br = new BufferedReader(
+                    new InputStreamReader(con.getInputStream(), StandardCharsets.UTF_8));
             while ((responseLine = br.readLine()) != null) {
                 response.append(responseLine.trim());
             }
+        } catch (MalformedURLException e) {
+            System.out.println(e.getMessage()+" Не доступен");
+        } catch (UnknownHostException e) {
+            System.out.println(e.getMessage()+" Не удается подключиться");
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
+
         return  response.toString() ;
     }
 
-    public static void main(String[] args) throws IOException {
-        RemoteRest remoteRest=new RemoteRest();
-        String response = remoteRest.getResponse("");
-        JSONObject jsonObject =new JSONObject(response);
-        JSONObject string = jsonObject.getJSONObject("result");
-        Object all = string.get("all");
-        System.out.println(all);
-    }
+
+//    public static void main(String[] args) throws IOException {
+//        RemoteRest remoteRest=new RemoteRest();
+//        String response = remoteRest.getResponse("dlg.stats_active");
+//        JSONObject jsonObject =new JSONObject(response);
+//        JSONObject string = jsonObject.getJSONObject("result");
+//        Object all = string.get("all");
+//        System.out.println(all);
+//    }
 }
